d6375a7e0874bd6fe2acd2a1bad656006d26606b
Bill Liza <billliza@hotmail.com>
2023-09-30 12:32:11 -0400

    Fixed redirect page to Sign Up page

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,12 +1,12 @@
 function showClick() {
     let msg = alert('You clicked me!')
-    window.location.replace("http://127.0.0.1:5500/logged.html")
+    window.location.replace("logged.html")
     console.log('Login successful!')
 
 }
 
 function signUp(){
     let msg = alert('Redirecting you to Sign Up.')
-    window.location.replace(signUp.html)
+    window.location.replace('signUp.html')
     console.log('Creating new username')
 }
